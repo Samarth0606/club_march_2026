063212d16b636dafa5a8903286e64be1de1ed4af
Samarth Vohra <samarthvohra@Samarths-MacBook-Air.local>
2026-04-19 19:58:48 +0530

    lecture 12 js code

---
+++ Lecture-12/conditions/app.js
@@ -0,0 +1,87 @@
+
+
+// if(true){
+//     console.log("hello");
+// }
+// console.log("bye");
+
+
+
+// // if(false){
+// if(true){
+//     console.log("balle balle");
+// }
+
+// --------------------------
+
+// if(false){
+//     console.log("bye");
+// }
+// else{
+//     console.log("hey hi"); 
+// }
+
+// --------------------------
+
+
+// if (10>20){
+//     console.log("10 is greater");
+// }else{
+//     console.log("20 is greater");
+// }
+
+// --------------------------
+
+// if (10<20){
+//     console.log("10 is greater");
+// }else{
+//     console.log("20 is greater");
+// }
+
+// --------------------------
+// laddering as well
+
+// if(){}
+// else if(){}
+// else if(){}
+// else if(){}
+// else if(){}
+// else{}
+
+// --------------------------
+// let marks = 45; // check pass fail
+// if(marks > 33){
+//     console.log("PASS");
+// }else{
+//     console.log("FAIL");
+// }
+
+// --------------------------
+// let marks = 33; // check grade
+
+// let marks = 95;
+// o - 90+
+// a - 80+
+// b - 70+
+// c - 50+
+// ELSE FAIL
+
+// if(marks>90){
+//     console.log("O");
+// }
+// else if(marks > 80){
+//     console.log("A");
+// }
+// else if(marks > 70){
+//     console.log("B");
+// }
+// else if(marks > 50){
+//     console.log("C");
+// }
+// else{
+//     console.log("FAIL"); 
+// }
+
+
+
+
---
+++ Lecture-12/js_2/app.js
@@ -0,0 +1,72 @@
+
+// let a = 10;
+// console.log(a);
+// console.log( typeof(a) );
+
+// --------------------------------
+
+// let a = 10;
+// let b = 20;
+
+// console.log( a+b );
+// console.log( a-b );
+// console.log( a*b ); //200
+// console.log( a/b ); // 10/20 = 0.5
+// console.log( a**4 ); // 10^20 = 10000
+// console.log( a%b); // 10%20 = 
+
+
+// --------------------------------
+
+
+// let str1 = "sam";
+// let str2 = 'sam';
+// let str3 = `sam`; //back-ticks
+
+// console.log(typeof str1);
+// console.log(typeof str2);
+// console.log(typeof str3);
+
+
+// --------------------------------
+
+// let str = "Hey there i am code baithak welcome to our youtube channel"
+// let str = "Hey there this is code baithak's youtube channel"
+// let str = "Hey there this is code baithak's youtube channel"
+
+// console.log(str);
+
+// --------------------------------
+
+// string template literals:
+
+// let occupation = "Youtuber"
+// let name = "Code Baithak"
+// let videos = 100
+
+// // Hey i am a youtuber my channel is Code Baithak where we have 100 videos
+
+// let output2 = "Hey i am a " + occupation + " my channel is " + name + " where we have " + videos + " videos"
+// console.log(output2);
+
+// // string template liteals
+
+
+// let output = `Hey i am a ${occupation} my channel is ${name} where we have ${videos} videos`;
+// console.log(output);
+
+
+// --------------------------------
+
+// let a = true;
+// let b = false;
+
+// console.log(a);
+// console.log(typeof a);
+
+// console.log(b);
+// console.log(typeof b);
+
+
+
+// --------------------------------
