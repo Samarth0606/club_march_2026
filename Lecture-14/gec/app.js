231e4dcdbab9f8bd7753ee7fc660dd720a1913b1
Samarth Vohra <samarthvohra@Samarths-MacBook-Air.local>
2026-04-24 21:03:02 +0530

    lecture 14 js code

---
+++ Lecture-14/gec/app.js
@@ -0,0 +1,50 @@
+
+
+// var a = 10;
+// console.log(a);
+
+// function sam(){
+//     console.log("i am don");
+// }
+// sam();
+
+
+// ---------------------------
+
+
+
+// console.log(a);
+// var a = 10;
+// let b = 20;
+// console.log(b);
+
+// function sam(){
+//     console.log(c);
+//     var c = 30;
+//     console.log(c);
+// }
+// sam();
+// console.log(a);
+
+// ---------------------------
+
+console.log(a);
+var a = 10;
+let b = 20;
+const c = 30;
+console.log(b);
+console.log(c);
+
+sam();
+
+function sam(){
+    console.log(d);
+    var d = 40;
+    let e = 50;
+    let f = 60;
+    console.log(e);
+    console.log(f);
+}
+
+console.log(a);
+
