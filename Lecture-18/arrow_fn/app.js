cf3c19ab6358748dfff6395135bb9da7090616d0
Samarth Vohra <samarthvohra@Samarths-MacBook-Air.local>
2026-05-10 20:13:03 +0530

    lecture 18 js code

---
+++ Lecture-18/arrow_fn/app.js
@@ -0,0 +1,31 @@
+// function square(n){
+// 	return n * n
+// }
+// console.log( square(6) )
+
+// ----------------------------
+
+// const square = (n)=>{
+// 	return n * n
+// }
+// console.log( square(6) )
+
+// ----------------------------
+// syntactical sugar
+
+const cube = (n)=>{
+    return n*n*n
+}
+// console.log(cube(5));
+
+// ----------------------------
+
+const cube2 = (n)=> n*n*n
+
+console.log(cube2(6));
+
+// ----------------------------
+
+const cube3 = n => n*n*n
+
+console.log(cube3(7));
--- Lecture-18/this/app.js
+++ Lecture-18/this/app.js
@@ -54,15 +54,60 @@
 // -------------------------
 // 3. constructor calling = newly created object
 
-function Sam(n , a){
-    this.name = n;
-    this.age = a;
-}
-let s1 = new Sam("mav" , 5)
-let s2 = new Sam("sam" , 12)
+// function Sam(n , a){
+//     this.name = n;
+//     this.age = a;
+// }
+// let s1 = new Sam("mav" , 5)
+// let s2 = new Sam("sam" , 12)
 
-console.log(s1);
-console.log(s2);
+// console.log(s1);
+// console.log(s2);
+
+// -------------------------
+// 5. arrow fn: this keyword is dependant on parents this
+
+
+// let obj = {
+//     a: 500,
+//     fun: function(){ 
+//         console.log(this , "1"); //obj       window
+//         let sam = ()=>{
+//             console.log(this , "2"); //obj   window
+//         }
+//         sam()
+//     }
+// }
+
+// // obj.fun()
+
+// let ans = obj.fun;
+// ans()
+
+// -------------------------
+// 4. indirect calling: call apply bind // changes the reference of this keyword.
+
+let obj= {
+    a: 10,
+    fn: function(x,y){
+        console.log(this.a , x , y);
+    }
+}
+obj.fn() //10
+
+let obj2= {
+    a: 50
+}
+
+// call
+obj.fn.call(obj2, 100 , 200) //new ref of this keyword - arguments are comma seperated
+obj.fn.apply(obj2, [100 , 200]) //new ref of this keyword - arguemnt are in array
+
+// bind - it creates a copy of that fn, it doesnot execute
+let newFn = obj.fn.bind(obj2, 100 , 200)
+newFn();
+
+
 
 
 
