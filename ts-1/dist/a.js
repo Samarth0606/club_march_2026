f9c42487de92fa63b72ee1d8697efd67ec9f3d01
Samarth Vohra <samarthvohra@Samarths-MacBook-Air.local>
2026-09-11 18:34:01 +0530

    ts-1

---
+++ ts-1/dist/a.js
@@ -0,0 +1,5 @@
+"use strict";
+let a = 10; //automatically accept
+// a = "sam"; //js ✅   ts ❌
+a = 10000;
+// inference = automatically maan lena values
---
+++ ts-1/src/a.ts
@@ -0,0 +1,14 @@
+// let a:number = 10 //automatically accept
+
+// a = "sam"; //js ✅   ts ❌
+// a = 10000;
+
+// inference = automatically maan lena values
+
+
+function sam(x){
+    return x
+}
+sam(100)
+
+
